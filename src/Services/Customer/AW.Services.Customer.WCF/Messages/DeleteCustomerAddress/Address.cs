8575fbaf82b4533f008123eb293927125c006305
Nils Gruson <ngruson@hotmail.com>
2021-06-17 16:53:38 +0200

    Sonar fixes

--- src/Services/Customer/AW.Services.Customer.WCF/Messages/DeleteCustomerAddress/Address.cs
+++ src/Services/Customer/AW.Services.Customer.WCF/Messages/DeleteCustomerAddress/Address.cs
@@ -10,8 +10,8 @@ namespace AW.Services.Customer.WCF.Messages.DeleteCustomerAddress
         public string AddressLine2 { get; set; }
         public string PostalCode { get; set; }
         public string City { get; set; }
-        public string StateProvince { get; set; }
-        public string Country { get; set; }
+        public string StateProvinceCode { get; set; }
+        public string CountryRegionCode { get; set; }
 
         public void Mapping(Profile profile)
         {
