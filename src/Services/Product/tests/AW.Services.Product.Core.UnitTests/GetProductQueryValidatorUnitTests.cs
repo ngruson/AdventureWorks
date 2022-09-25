d78784623b11c89861c4eb236ec13eb0243c4469
Nils Gruson <ngruson@hotmail.com>
2022-09-25 16:58:49 +0200

    Fixed unit tests

--- src/Services/Product/tests/AW.Services.Product.Core.UnitTests/GetProductQueryValidatorUnitTests.cs
+++ src/Services/Product/tests/AW.Services.Product.Core.UnitTests/GetProductQueryValidatorUnitTests.cs
@@ -7,6 +7,7 @@ using AW.SharedKernel.UnitTesting;
 using FluentValidation.TestHelper;
 using Moq;
 using System.Threading;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace AW.Services.Product.Core.UnitTests
@@ -14,53 +15,55 @@ namespace AW.Services.Product.Core.UnitTests
     public class GetProductQueryValidatorUnitTests
     {
         [Theory, AutoMapperData(typeof(MappingProfile))]
-        public void TestValidate_Valid_NoValidationError(
+        public async Task TestValidate_Valid_NoValidationError(
             GetProductQueryValidator sut,
             GetProductQuery query
         )
         {
             query.ProductNumber = query.ProductNumber.Substring(0, 25);
-            var result = sut.TestValidate(query);
+            var result = await sut.TestValidateAsync(query);
             result.ShouldNotHaveValidationErrorFor(query => query.ProductNumber);
         }
 
         [Theory, AutoMapperData(typeof(MappingProfile))]
-        public void TestValidate_QueryWithEmptyProductNumber_ValidationError(
+        public async Task TestValidate_QueryWithEmptyProductNumber_ValidationError(
             GetProductQueryValidator sut,
             GetProductQuery query
         )
         {
             query.ProductNumber = "";
-            var result = sut.TestValidate(query);
+            var result = await sut.TestValidateAsync(query);
             result.ShouldHaveValidationErrorFor(query => query.ProductNumber)
                 .WithErrorMessage("Product number is required");
         }
 
         [Theory, AutoMapperData(typeof(MappingProfile))]
-        public void TestValidate_QueryWithTooLongProductNumber_ValidationError(
+        public async Task TestValidate_QueryWithTooLongProductNumber_ValidationError(
             GetProductQueryValidator sut,
             GetProductQuery query
         )
         {
-            var result = sut.TestValidate(query);
+            var result = await sut.TestValidateAsync(query);
             result.ShouldHaveValidationErrorFor(query => query.ProductNumber)
                 .WithErrorMessage("Product number must not exceed 25 characters");
         }
 
         [Theory, AutoMapperData(typeof(MappingProfile))]
-        public void TestValidate_ProductNumberDoesNotExist_ValidationError(
+        public async Task TestValidate_ProductNumberDoesNotExist_ValidationError(
             [Frozen] Mock<IRepository<Entities.Product>> productRepoMock,
             GetProductQueryValidator sut,
             GetProductQuery query
         )
         {
+            query.ProductNumber = query.ProductNumber.Substring(0, 25);
+
             productRepoMock.Setup(x => x.SingleOrDefaultAsync(
                 It.IsAny<GetProductSpecification>(),
                 It.IsAny<CancellationToken>()
             ))
             .ReturnsAsync((Entities.Product)null);
 
-            var result = sut.TestValidate(query);
+            var result = await sut.TestValidateAsync(query);
             result.ShouldHaveValidationErrorFor(query => query.ProductNumber)
                 .WithErrorMessage("Product does not exist");
         }
