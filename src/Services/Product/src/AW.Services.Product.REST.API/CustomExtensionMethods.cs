b6b2b9a0eb3eab12dac79b0911d95a4f9ad5f24b
Nils Gruson <ngruson@hotmail.com>
2023-02-14 17:39:09 +0100

    Sonar fixes

--- src/Services/Product/src/AW.Services.Product.REST.API/CustomExtensionMethods.cs
+++ src/Services/Product/src/AW.Services.Product.REST.API/CustomExtensionMethods.cs
@@ -1,7 +1,6 @@
-using AW.Services.Infrastructure.Filters;
+﻿using AW.Services.Infrastructure.Filters;
 using AW.Services.Product.Core.Handlers.GetProducts;
 using AW.Services.Product.Infrastructure.EFCore.Configurations;
-using AW.Services.Product.REST.API;
 using AW.Services.SharedKernel.EFCore;
 using AW.Services.SharedKernel.Interfaces;
 using AW.SharedKernel.Api;
@@ -17,106 +16,109 @@ using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Identity.Web;
 using System.IdentityModel.Tokens.Jwt;
 
-public static class CustomExtensionMethods
+namespace AW.Services.Product.REST.API
 {
-    public static IServiceCollection AddCustomMvc(this IServiceCollection services)
+    public static class CustomExtensionMethods
     {
-        services.AddControllers(options =>
+        public static IServiceCollection AddCustomMvc(this IServiceCollection services)
         {
-            options.Filters.Add(typeof(HttpGlobalExceptionFilter));
-            options.Filters.Add(typeof(ValidateModelStateFilterAttribute));
-        });
+            services.AddControllers(options =>
+            {
+                options.Filters.Add(typeof(HttpGlobalExceptionFilter));
+                options.Filters.Add(typeof(ValidateModelStateFilterAttribute));
+            });
 
-        services
-            .AddFluentValidationAutoValidation()
-            .AddValidatorsFromAssemblyContaining<GetProductsQueryValidator>();
+            services
+                .AddFluentValidationAutoValidation()
+                .AddValidatorsFromAssemblyContaining<GetProductsQueryValidator>();
 
-        services.AddMvcCore()
-            .AddApiExplorer();
+            services.AddMvcCore()
+                .AddApiExplorer();
 
-        return services;
-    }
-
-    public static IServiceCollection AddVersioning(this IServiceCollection services)
-    {
-        services.AddApiVersioning(options => options.ReportApiVersions = true)
-            .AddVersionedApiExplorer(
-                options =>
-                {
-                    options.GroupNameFormat = "'v'VVV";
-                    options.SubstituteApiVersionInUrl = true;
-                }
-            );
-
-        return services;
-    }
-
-    public static IServiceCollection AddCustomAuthentication(this IServiceCollection services, IConfiguration configuration)
-    {
-        JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
-
-        if (configuration["AuthN:IdP"] == "AzureAd")
-        {
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddMicrosoftIdentityWebApi(configuration.GetSection("AuthN:AzureAd"));
-        }
-        else if (configuration["AuthN:IdP"] == "IdSrv")
-        {
-            var oidcConfig = new OpenIdConnectConfigurationBuilder(configuration).Build();
-
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(options =>
-                {
-                    options.Authority = oidcConfig?.Authority;
-                    options.Audience = "product-api";
-                    options.TokenValidationParameters.ValidTypes = new[] { "at+jwt" };
-                });
+            return services;
         }
 
-        return services;
-    }
-
-    public static IServiceCollection AddCustomSwagger(this IServiceCollection services)
-    {
-        services.AddSwaggerDocumentation("Product API");
-
-        return services;
-    }
-
-    public static IServiceCollection AddCustomIntegrations(this IServiceCollection services, IConfiguration configuration)
-    {
-        services.AddTransient(provider =>
+        public static IServiceCollection AddVersioning(this IServiceCollection services)
         {
-            var builder = new DbContextOptionsBuilder<AWContext>();
-            builder.UseSqlServer(configuration.GetConnectionString("DbConnection")!);
+            services.AddApiVersioning(options => options.ReportApiVersions = true)
+                .AddVersionedApiExplorer(
+                    options =>
+                    {
+                        options.GroupNameFormat = "'v'VVV";
+                        options.SubstituteApiVersionInUrl = true;
+                    }
+                );
 
-            return new AWContext(
-                provider.GetRequiredService<ILogger<AWContext>>(),
-                builder.Options,
-                provider.GetRequiredService<IMediator>(),
-                typeof(ProductConfiguration).Assembly
-            );
-        });
-        services.AddScoped(typeof(IRepository<>), typeof(EfRepository<>));
-        services.AddAutoMapper(typeof(MappingProfile).Assembly, typeof(GetProductsQuery).Assembly);
-        services.AddMediatR(typeof(GetProductsQuery));
-        services.AddScoped<IFileHandler, FileHandler>();
+            return services;
+        }
 
-        return services;
-    }
+        public static IServiceCollection AddCustomAuthentication(this IServiceCollection services, IConfiguration configuration)
+        {
+            JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
 
-    public static IServiceCollection AddCustomHealthCheck(this IServiceCollection services, IConfiguration configuration)
-    {
-        var hcBuilder = services.AddHealthChecks();
+            if (configuration["AuthN:IdP"] == "AzureAd")
+            {
+                services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                    .AddMicrosoftIdentityWebApi(configuration.GetSection("AuthN:AzureAd"));
+            }
+            else if (configuration["AuthN:IdP"] == "IdSrv")
+            {
+                var oidcConfig = new OpenIdConnectConfigurationBuilder(configuration).Build();
 
-        hcBuilder.AddCheck("self", () => HealthCheckResult.Healthy());
-        hcBuilder.AddElasticsearch(configuration["ElasticSearchUri"]!);
+                services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                    .AddJwtBearer(options =>
+                    {
+                        options.Authority = oidcConfig?.Authority;
+                        options.Audience = "product-api";
+                        options.TokenValidationParameters.ValidTypes = new[] { "at+jwt" };
+                    });
+            }
 
-        if (configuration["AuthN:IdP"] == "IdSrv")
-            hcBuilder.AddIdentityServer(new Uri(configuration["AuthN:IdSrv:Authority"]!));
+            return services;
+        }
 
-        hcBuilder.AddSqlServer(configuration.GetConnectionString("DbConnection")!);
+        public static IServiceCollection AddCustomSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerDocumentation("Product API");
 
-        return services;
+            return services;
+        }
+
+        public static IServiceCollection AddCustomIntegrations(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddTransient(provider =>
+            {
+                var builder = new DbContextOptionsBuilder<AWContext>();
+                builder.UseSqlServer(configuration.GetConnectionString("DbConnection")!);
+
+                return new AWContext(
+                    provider.GetRequiredService<ILogger<AWContext>>(),
+                    builder.Options,
+                    provider.GetRequiredService<IMediator>(),
+                    typeof(ProductConfiguration).Assembly
+                );
+            });
+            services.AddScoped(typeof(IRepository<>), typeof(EfRepository<>));
+            services.AddAutoMapper(typeof(MappingProfile).Assembly, typeof(GetProductsQuery).Assembly);
+            services.AddMediatR(typeof(GetProductsQuery));
+            services.AddScoped<IFileHandler, FileHandler>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddCustomHealthCheck(this IServiceCollection services, IConfiguration configuration)
+        {
+            var hcBuilder = services.AddHealthChecks();
+
+            hcBuilder.AddCheck("self", () => HealthCheckResult.Healthy());
+            hcBuilder.AddElasticsearch(configuration["ElasticSearchUri"]!);
+
+            if (configuration["AuthN:IdP"] == "IdSrv")
+                hcBuilder.AddIdentityServer(new Uri(configuration["AuthN:IdSrv:Authority"]!));
+
+            hcBuilder.AddSqlServer(configuration.GetConnectionString("DbConnection")!);
+
+            return services;
+        }
     }
 }
--- src/Services/Sales/src/AW.Services.Sales.SalesPerson.REST.API/CustomExtensionMethods.cs
+++ src/Services/Sales/src/AW.Services.Sales.SalesPerson.REST.API/CustomExtensionMethods.cs
@@ -1,4 +1,4 @@
-using AW.Services.Infrastructure;
+﻿using AW.Services.Infrastructure;
 using AW.Services.Infrastructure.EventBus;
 using AW.Services.Infrastructure.EventBus.Abstractions;
 using AW.Services.Infrastructure.EventBus.AzureServiceBus;
@@ -9,7 +9,6 @@ using AW.Services.Sales.Core.Handlers.GetSalesPersons;
 using AW.Services.Sales.Core.IntegrationEvents;
 using AW.Services.Sales.Core.IntegrationEvents.Events;
 using AW.Services.Sales.Infrastructure.EFCore.Configurations;
-using AW.Services.Sales.SalesPerson.REST.API;
 using AW.Services.SharedKernel.EFCore;
 using AW.Services.SharedKernel.Interfaces;
 using AW.SharedKernel.Api;
@@ -21,196 +20,199 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Identity.Web;
 
-static class CustomExtensionMethods
+namespace AW.Services.Sales.SalesPerson.REST.API
 {
-    public static IServiceCollection AddCustomMvc(this IServiceCollection services)
+    static class CustomExtensionMethods
     {
-        services.AddControllers(options =>
+        public static IServiceCollection AddCustomMvc(this IServiceCollection services)
         {
-            options.Filters.Add(typeof(HttpGlobalExceptionFilter));
-            options.Filters.Add(typeof(ValidateModelStateFilterAttribute));
-        });
+            services.AddControllers(options =>
+            {
+                options.Filters.Add(typeof(HttpGlobalExceptionFilter));
+                options.Filters.Add(typeof(ValidateModelStateFilterAttribute));
+            });
 
-        return services;
-    }
-
-    public static IServiceCollection AddVersioning(this IServiceCollection services)
-    {
-        services.AddMvcCore()
-            .AddApiExplorer();
-
-        services.AddApiVersioning(options => options.ReportApiVersions = true)
-            .AddVersionedApiExplorer(
-                options =>
-                {
-                    options.GroupNameFormat = "'v'VVV";
-                    options.SubstituteApiVersionInUrl = true;
-                }
-            );
-
-        return services;
-    }
-
-    public static IServiceCollection AddCustomAuthentication(this IServiceCollection services, IConfiguration configuration)
-    {
-        if (configuration["AuthN:IdP"] == "AzureAd")
-        {
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddMicrosoftIdentityWebApi(configuration.GetSection("AuthN:AzureAd"));
+            return services;
         }
-        else if (configuration["AuthN:IdP"] == "IdSrv")
-        {
-            var oidcConfig = new OpenIdConnectConfigurationBuilder(configuration).Build();
 
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(options =>
+        public static IServiceCollection AddVersioning(this IServiceCollection services)
+        {
+            services.AddMvcCore()
+                .AddApiExplorer();
+
+            services.AddApiVersioning(options => options.ReportApiVersions = true)
+                .AddVersionedApiExplorer(
+                    options =>
+                    {
+                        options.GroupNameFormat = "'v'VVV";
+                        options.SubstituteApiVersionInUrl = true;
+                    }
+                );
+
+            return services;
+        }
+
+        public static IServiceCollection AddCustomAuthentication(this IServiceCollection services, IConfiguration configuration)
+        {
+            if (configuration["AuthN:IdP"] == "AzureAd")
+            {
+                services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                    .AddMicrosoftIdentityWebApi(configuration.GetSection("AuthN:AzureAd"));
+            }
+            else if (configuration["AuthN:IdP"] == "IdSrv")
+            {
+                var oidcConfig = new OpenIdConnectConfigurationBuilder(configuration).Build();
+
+                services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                    .AddJwtBearer(options =>
+                    {
+                        options.Authority = oidcConfig?.Authority;
+                        options.Audience = "salesperson-api";
+                        options.TokenValidationParameters.ValidTypes = new[] { "at+jwt" };
+                    });
+            }
+
+            return services;
+        }
+
+        public static IServiceCollection AddCustomSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerDocumentation("Sales Person API");
+
+            return services;
+        }
+
+        public static IServiceCollection AddCustomDbContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped(provider =>
+            {
+                var builder = new DbContextOptionsBuilder<AWContext>();
+                builder.UseSqlServer(configuration.GetConnectionString("DbConnection"));
+
+                return new AWContext(
+                    provider.GetRequiredService<ILogger<AWContext>>(),
+                    builder.Options,
+                    provider.GetRequiredService<IMediator>(),
+                    typeof(SalesPersonConfiguration).Assembly
+                );
+            });
+
+            return services;
+        }
+
+        public static IServiceCollection AddEventBus(this IServiceCollection services, IConfiguration configuration)
+        {
+            if (configuration.GetValue<bool>("AzureServiceBusEnabled"))
+            {
+                services.AddSingleton<IEventBus, EventBusServiceBus>(sp =>
                 {
-                    options.Authority = oidcConfig?.Authority;
-                    options.Audience = "salesperson-api";
-                    options.TokenValidationParameters.ValidTypes = new[] { "at+jwt" };
+                    var serviceBusPersisterConnection = sp.GetRequiredService<IServiceBusPersisterConnection>();
+                    var logger = sp.GetRequiredService<ILogger<EventBusServiceBus>>();
+                    var eventBusSubcriptionsManager = sp.GetRequiredService<IEventBusSubscriptionsManager>();
+                    var topicName = configuration["EventBusTopic"];
+
+                    return new EventBusServiceBus(
+                        sp,
+                        serviceBusPersisterConnection,
+                        logger,
+                        eventBusSubcriptionsManager,
+                        topicName!
+                    );
                 });
-        }
-
-        return services;
-    }
-
-    public static IServiceCollection AddCustomSwagger(this IServiceCollection services)
-    {
-        services.AddSwaggerDocumentation("Sales Person API");
-
-        return services;
-    }
-
-    public static IServiceCollection AddCustomDbContext(this IServiceCollection services, IConfiguration configuration)
-    {
-        services.AddScoped(provider =>
-        {
-            var builder = new DbContextOptionsBuilder<AWContext>();
-            builder.UseSqlServer(configuration.GetConnectionString("DbConnection"));
-
-            return new AWContext(
-                provider.GetRequiredService<ILogger<AWContext>>(),
-                builder.Options,
-                provider.GetRequiredService<IMediator>(),
-                typeof(SalesPersonConfiguration).Assembly
-            );
-        });
-
-        return services;
-    }
-
-    public static IServiceCollection AddEventBus(this IServiceCollection services, IConfiguration configuration)
-    {
-        if (configuration.GetValue<bool>("AzureServiceBusEnabled"))
-        {
-            services.AddSingleton<IEventBus, EventBusServiceBus>(sp =>
+            }
+            else
             {
-                var serviceBusPersisterConnection = sp.GetRequiredService<IServiceBusPersisterConnection>();
-                var logger = sp.GetRequiredService<ILogger<EventBusServiceBus>>();
-                var eventBusSubcriptionsManager = sp.GetRequiredService<IEventBusSubscriptionsManager>();
-                var topicName = configuration["EventBusTopic"];
-
-                return new EventBusServiceBus(
-                    sp,
-                    serviceBusPersisterConnection,
-                    logger,
-                    eventBusSubcriptionsManager,
-                    topicName!
-                );
-            });
-        }
-        else
-        {
-            services.AddSingleton<IEventBus, EventBusRabbitMQ>(sp =>
-            {
-                var rabbitMQPersistentConnection = sp.GetRequiredService<IRabbitMQPersistentConnection>();
-                var logger = sp.GetRequiredService<ILogger<EventBusRabbitMQ>>();
-                var eventBusSubcriptionsManager = sp.GetRequiredService<IEventBusSubscriptionsManager>();
-
-                var queueName = configuration["EventBusQueueName"];
-                var retryCount = 5;
-                if (!string.IsNullOrEmpty(configuration["EventBusRetryCount"]))
+                services.AddSingleton<IEventBus, EventBusRabbitMQ>(sp =>
                 {
-                    retryCount = int.Parse(configuration["EventBusRetryCount"]!);
-                }
+                    var rabbitMQPersistentConnection = sp.GetRequiredService<IRabbitMQPersistentConnection>();
+                    var logger = sp.GetRequiredService<ILogger<EventBusRabbitMQ>>();
+                    var eventBusSubcriptionsManager = sp.GetRequiredService<IEventBusSubscriptionsManager>();
 
-                return new EventBusRabbitMQ(sp.GetRequiredService<IServiceScopeFactory>(),
-                    rabbitMQPersistentConnection,
-                    logger,
-                    eventBusSubcriptionsManager,
-                    queueName!, 
-                    retryCount
-                );
-            });
+                    var queueName = configuration["EventBusQueueName"];
+                    var retryCount = 5;
+                    if (!string.IsNullOrEmpty(configuration["EventBusRetryCount"]))
+                    {
+                        retryCount = int.Parse(configuration["EventBusRetryCount"]!);
+                    }
+
+                    return new EventBusRabbitMQ(sp.GetRequiredService<IServiceScopeFactory>(),
+                        rabbitMQPersistentConnection,
+                        logger,
+                        eventBusSubcriptionsManager,
+                        queueName!,
+                        retryCount
+                    );
+                });
+            }
+
+            services.AddSingleton<IEventBusSubscriptionsManager, InMemoryEventBusSubscriptionsManager>();
+
+            return services;
         }
 
-        services.AddSingleton<IEventBusSubscriptionsManager, InMemoryEventBusSubscriptionsManager>();
-
-        return services;
-    }
-
-    public static IServiceCollection AddCustomIntegrations(this IServiceCollection services, IConfiguration configuration)
-    {
-        services.AddScoped(typeof(IRepository<>), typeof(EfRepository<>));
-        services.AddAutoMapper(typeof(MappingProfile).Assembly, typeof(GetSalesPersonsQuery).Assembly);
-        services.AddMediatR(typeof(GetSalesPersonsQuery));
-
-        services.AddScoped(typeof(IApplication), typeof(Application));
-        services.AddScoped<ISalesOrderIntegrationEventService, SalesOrderIntegrationEventService>();
-        services.AddIntegrationEventHandlers();
-        services.AddScoped<IDbContext>(sp => sp.GetRequiredService<AWContext>());
-        services.AddScoped<IIntegrationEventLogService>(sp =>
+        public static IServiceCollection AddCustomIntegrations(this IServiceCollection services, IConfiguration configuration)
         {
-            var dbContext = sp.GetRequiredService<AWContext>();
-            return new IntegrationEventLogService(
-                dbContext,
-                typeof(OrderStartedIntegrationEvent).Assembly
-            );
-        });
+            services.AddScoped(typeof(IRepository<>), typeof(EfRepository<>));
+            services.AddAutoMapper(typeof(MappingProfile).Assembly, typeof(GetSalesPersonsQuery).Assembly);
+            services.AddMediatR(typeof(GetSalesPersonsQuery));
 
-        return services;
-    }
+            services.AddScoped(typeof(IApplication), typeof(Application));
+            services.AddScoped<ISalesOrderIntegrationEventService, SalesOrderIntegrationEventService>();
+            services.AddIntegrationEventHandlers();
+            services.AddScoped<IDbContext>(sp => sp.GetRequiredService<AWContext>());
+            services.AddScoped<IIntegrationEventLogService>(sp =>
+            {
+                var dbContext = sp.GetRequiredService<AWContext>();
+                return new IntegrationEventLogService(
+                    dbContext,
+                    typeof(OrderStartedIntegrationEvent).Assembly
+                );
+            });
 
-    public static IServiceCollection AddIntegrationEventHandlers(this IServiceCollection services)
-    {
-        var coreAssembly = typeof(GetSalesPersonsQuery).Assembly;
-        var types = coreAssembly.GetExportedTypes()
-            .Where(t => t.GetInterfaces().Any(i =>
-                i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IIntegrationEventHandler<>))
-            )
-            .ToList();
+            return services;
+        }
 
-        types.ForEach(type =>
+        public static IServiceCollection AddIntegrationEventHandlers(this IServiceCollection services)
         {
-            var interfaces = type.GetInterfaces()
-                .Where(i =>
-                    i.IsGenericType &&
-                    i.GetGenericTypeDefinition() == typeof(IIntegrationEventHandler<>)
+            var coreAssembly = typeof(GetSalesPersonsQuery).Assembly;
+            var types = coreAssembly.GetExportedTypes()
+                .Where(t => t.GetInterfaces().Any(i =>
+                    i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IIntegrationEventHandler<>))
                 )
                 .ToList();
 
-            // Has the type implemented any IIntegrationEventHandler<T>?
-            if (interfaces.Count > 0)
+            types.ForEach(type =>
             {
-                services.AddScoped(interfaces[0], type);
-            }
-        });
+                var interfaces = type.GetInterfaces()
+                    .Where(i =>
+                        i.IsGenericType &&
+                        i.GetGenericTypeDefinition() == typeof(IIntegrationEventHandler<>)
+                    )
+                    .ToList();
 
-        return services;
-    }
+                // Has the type implemented any IIntegrationEventHandler<T>?
+                if (interfaces.Count > 0)
+                {
+                    services.AddScoped(interfaces[0], type);
+                }
+            });
 
-    public static IServiceCollection AddCustomHealthCheck(this IServiceCollection services, IConfiguration configuration)
-    {
-        var hcBuilder = services.AddHealthChecks();
+            return services;
+        }
 
-        hcBuilder.AddCheck("self", () => HealthCheckResult.Healthy());
-        hcBuilder.AddElasticsearch(configuration["ElasticSearchUri"]!);
+        public static IServiceCollection AddCustomHealthCheck(this IServiceCollection services, IConfiguration configuration)
+        {
+            var hcBuilder = services.AddHealthChecks();
 
-        if (configuration["AuthN:IdP"] == "IdSrv")
-            hcBuilder.AddIdentityServer(new Uri(configuration["AuthN:IdSrv:Authority"]!));
+            hcBuilder.AddCheck("self", () => HealthCheckResult.Healthy());
+            hcBuilder.AddElasticsearch(configuration["ElasticSearchUri"]!);
 
-        hcBuilder.AddSqlServer(configuration.GetConnectionString("DbConnection")!);
+            if (configuration["AuthN:IdP"] == "IdSrv")
+                hcBuilder.AddIdentityServer(new Uri(configuration["AuthN:IdSrv:Authority"]!));
 
-        return services;
+            hcBuilder.AddSqlServer(configuration.GetConnectionString("DbConnection")!);
+
+            return services;
+        }
     }
 }
